9d5a78832aca698600692e6834cd30e0eabaa6a1
Alessandro <alessandro@rivalex.com>
2015-10-20 16:11:25 +0200

    demo

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,15 @@
 $('.passMeter').bootstrapStrength();
 $('.passMeterSlim').bootstrapStrength({
-	slimBar: false
+	slimBar: true
+});
+$('.passMeterStrip').bootstrapStrength({
+	strip: false,
+	stripped: false
+});
+$('.passMeterCustom').bootstrapStrength({
+	minLenght: 12,		// minimum lenght required
+	upperCase: 2,		// at least 2 Upper case letter
+	lowerCase: 3,		// at least 3 Lower case letter
+	numbers: 4,			// at least 4 Numbers
+	specialchars: 2		// at least 2 Special Characters
 });
